713c9ccb7f3a419b0c866807e27461b2f340541c
nastyaergenova <kissa2004kissa2004@mail.ru>
2024-10-15 12:32:39 +0300

    Task13: Добавлен метод Delete для удаления фильмов.

--- MovieApp_Git/Controllers/MoviesController.cs
+++ MovieApp_Git/Controllers/MoviesController.cs
@@ -133,6 +133,46 @@ namespace MovieApp.Controllers
             return View(movie);
         }
 
+        // GET: Movies/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movies
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        // POST: Movies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var movie = await _context.Movies.FindAsync(id);
+            if (movie != null)
+            {
+                _context.Movies.Remove(movie);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MovieExists(int id)
+        {
+            return _context.Movies.Any(e => e.Id == id);
+        }
+
     }
+
+
 
 }
