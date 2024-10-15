bfce0e258013e8804b1390cbb78edf26e165ad3c
nastyaergenova <kissa2004kissa2004@mail.ru>
2024-10-15 11:42:51 +0300

    Настроен репозиторий

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieApp_Git.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
