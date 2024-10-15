eb935c20178b9249a7e2d33908321df90eb2c81e
nastyaergenova <kissa2004kissa2004@mail.ru>
2024-10-15 12:02:28 +0300

    #Task5: Реализован контроллер MoviesController с базовыми методами.

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieApp.Data;
+using MovieApp.Models;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace MovieApp.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly MovieContext _context;
+        private readonly IWebHostEnvironment _appEnvironment;
+
+        public MoviesController(MovieContext context, IWebHostEnvironment appEnvironment)
+        {
+            _context = context;
+            _appEnvironment = appEnvironment;
+        }
+
+        // GET: Movies
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Movies.ToListAsync());
+        }
+
+        // GET: Movies/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movies
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+    }
+}
